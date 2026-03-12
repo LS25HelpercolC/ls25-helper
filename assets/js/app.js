f4570cd20e7ac397f422cbcfc3c8c3dd1e06185a
LS25HelpercolC <schreinerfrank90@gmail.com>
2026-03-12 09:50:31 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,38 +22,18 @@ function buildCalendar(){
   const item = document.createElement("div")
   item.className = "calendarItem"
 
-  const sow = cropsData[crop].sow.join("–")
-  const harvest = cropsData[crop].harvest.join("–")
+  const sow = cropsData[crop].sow.join("-")
+  const harvest = cropsData[crop].harvest.join("-")
 
   item.innerHTML = `
-  <span>${crop}</span>
-  <span>${sow}</span>
-  <span>${harvest}</span>
+   <span>${crop}</span>
+   <span>${sow}</span>
+   <span>${harvest}</span>
   `
 
   calendar.appendChild(item)
 
  })
-
-}
-
-function buildTimeline(crop){
-
- const data = cropsData[crop]
-
- if(!data) return
-
- const timeline = document.getElementById("timeline")
-
- const row = document.createElement("div")
- row.className = "timelineRow"
-
- row.innerHTML = `
- <div class="timelineLabel">${crop}</div>
- <div class="timelineBar"></div>
- `
-
- timeline.appendChild(row)
 
 }
 
